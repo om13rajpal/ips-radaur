0cc930133ac563f397267b3710ed016cc7fd8f76
om rajpal <omrajpal.exe@gmail.com>
2025-03-19 22:28:57 +0530

    added search functionality

--- web/src/pages/Dashboard.tsx
+++ web/src/pages/Dashboard.tsx
@@ -1,14 +1,22 @@
 import EmployeeCard from "@/components/EmployeeCard";
+import { Input } from "@/components/ui/input";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { toast, Toaster } from "sonner";
 
 const Dashboard = () => {
   const [data, setData] = useState([]);
+  const [searchedData, setSeaerchedData] = useState("");
 
   useEffect(() => {
     fetchData();
   }, []);
+
+  const filteredData = data.filter((employee: any) => {
+    if (employee.name.toLowerCase().includes(searchedData.toLowerCase())) {
+      return employee;
+    }
+  });
 
   async function fetchData() {
     try {
@@ -34,10 +42,20 @@ const Dashboard = () => {
           No employee data
         </div>
       ) : (
-        <div className="w-[75vw] grid grid-cols-3 gap-4">
-          {data.map((employee: any, index: number) => {
-            return <EmployeeCard key={index} data={employee} />;
-          })}
+        <div>
+          <Input
+            placeholder="Type here to search ..."
+            type="text"
+            className="w-1/3"
+            onChange={(e) => {
+              setSeaerchedData(e.target.value);
+            }}
+          />
+          <div className="w-[75vw] grid grid-cols-3 gap-4 mt-5">
+            {filteredData.map((employee: any, index: number) => {
+              return <EmployeeCard key={index} data={employee} />;
+            })}
+          </div>
         </div>
       )}
       <Toaster />
